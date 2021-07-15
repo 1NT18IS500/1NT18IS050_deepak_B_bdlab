21bb2c99ce12f1eb13a7f748ffcb17f46f6fb48e
1NT18IS500 <83110250+1NT18IS500@users.noreply.github.com>
2021-07-16 03:15:09 +0530

    Add files via upload

---
+++ InternalBD/payRaise.java
@@ -0,0 +1,56 @@
+package deepak.pack.example;
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class  ComulativeAwards{
+
+  //MAPPER CODE	
+
+  public static class Map extends MapReduceBase implements Mapper < LongWritable, Text, Text, IntWritable > {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(LongWritable key, Text value, OutputCollector < Text, IntWritable > output, Reporter reporter) throws IOException {
+    	String myvalue = value.toString();
+    	String[] tokens = myvalue.split(",");
+    	
+    	if (tokens[4].equals("YES")) {	
+    		output.collect(new Text("Total number of employees eligible for pay raise: "), one);
+    	}
+    }
+  }
+
+  //REDUCER CODE	
+  public static class Reduce extends MapReduceBase implements Reducer < Text, IntWritable, Text, IntWritable > {
+    public void reduce(Text key, Iterator < IntWritable > values, OutputCollector < Text, IntWritable > output, Reporter reporter) throws IOException { //{little: {1,1}} 
+    	int count = 0 ; 
+    	while(values.hasNext()) {
+    		count += values.next().get();
+    	}
+    	output.collect(key, new IntWritable(count));
+
+    }
+  }
+
+  //DRIVER CODE
+  public static void main(String[] args) throws Exception {
+    JobConf conf = new JobConf(ComulativeAwards.class);
+    conf.setJobName("Prog1");
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(IntWritable.class);
+    conf.setMapperClass(Map.class);
+    conf.setCombinerClass(Reduce.class);
+    conf.setReducerClass(Reduce.class);
+    conf.setInputFormat(TextInputFormat.class);
+    conf.setOutputFormat(TextOutputFormat.class);
+    FileInputFormat.setInputPaths(conf, new Path(args[0]));
+    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+    JobClient.runJob(conf);
+  }
+}
